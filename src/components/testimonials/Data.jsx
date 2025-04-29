f67aafd8a2de592731527c8939b8c34575be5564
Gerald0225 <geraldprio84@gmail.com>
2025-04-29 12:43:05 +0800

    Add files via upload

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,22 @@
+import Image1 from "../../assets/man.png";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Kyle D.",
+        description: "Executed the concept with clarity and adaptability.",
+    },
+    {
+        id: 2,
+        image: Image1,
+        title: "Lennon J.",
+        description: "Provided creative insight and improvment to the project.",
+    },
+    {
+        id: 3,
+        image: Image1,
+        title: "John Allen B.",
+        description: "Fixed the glitch and made the site easier to use.",
+    },
+];
